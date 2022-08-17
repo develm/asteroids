610eedb347aa6bce35538b908325874cd2f0f3c7
Milos Zivkovic <milos.zivkovic@avawomen.com>
2022-08-17 23:42:26 +0200

    change window size

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod asset_manager;
 mod components;
 
 use bevy::prelude::*;
+use bevy::window::WindowMode;
 use crate::asset_manager::{AssetManagerPlugin, AtlasManager};
 use crate::components::Player;
 
@@ -9,8 +10,8 @@ fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
             title: "Asteroids".to_string(),
-            width: 640.0,
-            height: 480.0,
+            width: 1024.0,
+            height: 768.0,
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
@@ -27,7 +28,7 @@ fn load_game(
     commands.spawn()
         .insert_bundle(SpriteSheetBundle {
             texture_atlas: atlas_manager.texture_atlas.clone(),
-            transform: Transform::from_translation(Vec3::new(0.0, 220.0, 0.0)),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)).with_scale(Vec3::new(0.5, 0.5, 0.5)),
             sprite: TextureAtlasSprite::new(*atlas_manager.texture_index.get("spaceship").expect("Could not find texture")),
             ..Default::default()
         })
