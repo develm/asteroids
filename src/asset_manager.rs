20841a51d0b409882a022a4e716868c4d3244905
Milos Zivkovic <milos.zivkovic@avawomen.com>
2022-08-17 18:24:59 +0200

    asset_manager.rs implementation

---
+++ src/asset_manager.rs
@@ -0,0 +1,69 @@
+use std::fs::File;
+use ron::de::from_reader;
+use bevy::prelude::*;
+use bevy::sprite::Rect;
+use bevy::utils::HashMap;
+use serde::Deserialize;
+
+#[derive(Clone, Deserialize, Debug)]
+struct Sprite {
+    name: String,
+    position: (f32, f32, f32, f32),
+}
+
+#[derive(Clone, Deserialize, Debug)]
+struct SpriteSheet {
+    width: f32,
+    height: f32,
+    sprites: Vec<Sprite>,
+}
+
+impl SpriteSheet {
+    fn load() -> Self {
+        let file = File::open("assets/atlas.ron").expect("Failed to open atlas.ron file");
+        from_reader(file).expect("Could not deserialize atlas.ron")
+    }
+}
+
+pub struct AtlasManager {
+    pub texture_atlas: TextureAtlas,
+    pub texture_index: HashMap<String, i32>,
+}
+
+pub struct AssetManagerPlugin;
+
+impl Plugin for AssetManagerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(AssetManagerPlugin::load_assets);
+    }
+}
+
+impl AssetManagerPlugin {
+    fn load_assets(
+        mut commands: Commands,
+        asset_server: Res<AssetServer>,
+    ) {
+        let sprite_sheet = SpriteSheet::load();
+        let texture_handle = asset_server.load("");
+
+
+        let mut texture_atlas = TextureAtlas::new_empty(texture_handle, Vec2::new(sprite_sheet.width, sprite_sheet.height));
+
+        let mut texture_index = HashMap::new();
+        for (i, sprite) in sprite_sheet.sprites.iter().enumerate() {
+            texture_index.insert(sprite.name.clone(), i as i32);
+            let (x, y, w, h) = sprite.position;
+            texture_atlas.add_texture(
+                Rect {
+                    min: Vec2::new(x, y),
+                    max: Vec2::new(w, h),
+                });
+        }
+        commands.insert_resource(AtlasManager{
+            texture_atlas,
+            texture_index,
+        });
+    }
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod asset_manager;
+
+use bevy::prelude::*;
+use crate::asset_manager::AssetManagerPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(AssetManagerPlugin)
+        .run();
 }
